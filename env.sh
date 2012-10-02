291cd06660c2bb76c415fbcab8a5be69b7888d1a
Preston Koprivica <pkopriv2@gmail.com>
2012-10-01 23:25:44 -0500

    Uplifting to bashum support

--- env.sh
+++
@@ -1,6 +0,0 @@
-if [[ -z $bashee_home ]]
-then
-	export bashee_home=$HOME/.bashee
-fi
-
-PATH=$PATH:$bashee_home/bin
---
+++ project.sh
@@ -0,0 +1,6 @@
+name    "bashee"
+version "1.0.0"
+author  "Preston Koprivica"
+email   "pkopriv2@gmail.com"
+
+depends "bash-coreutils" 
